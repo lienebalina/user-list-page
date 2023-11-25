47556406e14ba00941728e072c786fec6f2270d6
Liene <Liene@LAPTOP-C1G7S8GH>
2023-11-25 22:02:37 +0200

    multiple textbox add/removal

--- user-list-page/user-list-page/Controllers/UserController.cs
+++ user-list-page/user-list-page/Controllers/UserController.cs
@@ -18,13 +18,6 @@ namespace user_list_page.Controllers
 
         public IActionResult Index(List<User> user)
         {
-            /*userViewModel = (from u in _context.Users
-                                 join a in _context.Address on u.Id equals a.UserId into ua
-                                 from a in ua.DefaultIfEmpty()
-                                 join p in _context.PhoneNumbers on u.Id equals p.UserId into upn
-                                 from p in upn.DefaultIfEmpty()
-                             select new UserViewModel { UsersViewModel = u, AddressViewModel = a, PhoneNumberViewModel = p })
-                                .ToList();*/
             user = _context.Users.Include(u => u.Addresses)
                 .Include(p => p.PhoneNumbers).ToList();
             
